11435eb56bc98e4939f216db7cb7d35e2b954749
d-crypto99 <79061002+d-crypto99@users.noreply.github.com>
2022-04-25 21:04:31 +0700

    Update rmdh.sh

--- rmdh.sh
+++ rmdh.sh
@@ -11,9 +11,11 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 185.212.160.70:7085
-socks5_username = lebarankita
-socks5_password = buatTHRan
+listen = :2233
+loglevel = 1
+socks5 = 45.9.154.251:12324
+socks5_username = 14a5029e51675
+socks5_password = 9d332fa7c0
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
